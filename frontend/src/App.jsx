e06fc91ad3ed00461a4720110614dc7650a6f3b3
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-10-29 14:21:41 -0600

    Footer Added, Page title and logo change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
-      <footer>This is the footer</footer>
+      <Footer />
     </>
   );
 };
