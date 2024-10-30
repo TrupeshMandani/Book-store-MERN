3a60405df59767b979be3168a2a268a54e08446e
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-10-30 17:55:42 -0600

    Bookcard fixed Fromatting

--- frontend/src/pages/books/BookCard.jsx
+++ frontend/src/pages/books/BookCard.jsx
@@ -10,47 +10,39 @@ const BookCard = ({ book }) => {
   const handleAddToCart = (product) => {
     dispatch(addToCart(product));
   };
-  return (
-    <div>
-      <div className=" rounded-lg transition-shadow duration-300">
-        <div className="flex flex-col sm:flex-row sm:items-center sm:h-72  sm:justify-center gap-4">
-          <div className="sm:h-72 sm:flex-shrink-0 border rounded-md">
-            <a href="/">
-              <img
-                src={`${getImgUrl(book.coverImage)}`}
-                alt=""
-                className="w-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
-              />
-            </a>
-          </div>
 
-          <div>
-            <Link to={`/books/${book._id}`}>
-              <h3 className="text-xl font-semibold hover:text-blue-600 mb-3">
-                {book.title}
-              </h3>
-            </Link>
-            <p className="text-gray-600 mb-5">
-              {book.description.length > 80
-                ? `${book.description.slice(0, 80)}...`
-                : book.description}
-            </p>
-            <p className="font-medium mb-5">
-              ${book.newPrice}{" "}
-              <span className="line-through font-normal ml-2">
-                ${book.oldPrice}
-              </span>
-            </p>
-            <button
-              onClick={() => {
-                handleAddToCart(book);
-              }}
-              className="btn-primary px-6 space-x-1 flex items-center gap-1 "
-            >
-              <FiShoppingCart className="" />
-              <span>Add to Cart</span>
-            </button>
-          </div>
+  return (
+    <div className="rounded-lg transition-shadow duration-300 h-72 sm:h-80 flex flex-col justify-between p-4">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:h-full sm:justify-center gap-4">
+        <div className="sm:h-full sm:flex-shrink-0 border rounded-md">
+          <a href="/">
+            <img
+              src={`${getImgUrl(book.coverImage)}`}
+              alt=""
+              className="w-full h-48 sm:h-full bg-cover p-2 rounded-md cursor-pointer hover:scale-105 transition-all duration-200"
+            />
+          </a>
+        </div>
+        <div className="sm:h-full flex flex-col justify-between">
+          <Link to={`/books/${book._id}`}>
+            <h3 className="text-xl font-semibold hover:text-blue-600 mb-1 ">
+              {book.title}
+            </h3>
+          </Link>
+          <p className="text-gray-600 mb-1 line-clamp-3">{book.description}</p>
+          <p className="font-medium mb-5">
+            ${book.newPrice}
+            <span className="line-through font-normal ml-2">
+              ${book.oldPrice}
+            </span>
+          </p>
+          <button
+            onClick={() => handleAddToCart(book)}
+            className="btn-primary w-full py-3 flex items-center justify-center gap-2 whitespace-nowrap"
+          >
+            <FiShoppingCart className="text-lg" />
+            <span>Add to Cart</span>
+          </button>
         </div>
       </div>
     </div>
