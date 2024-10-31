014373e1fecc32ca4c683213a0dd645587a97691
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-10-30 18:56:31 -0600

    The checkout page is done ( raw again )

--- frontend/src/pages/books/CheckoutPage.jsx
+++ frontend/src/pages/books/CheckoutPage.jsx
@@ -1,67 +1,233 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { removeFromCart, clearCart } from "../../redux/features/cart/cartSlice";
-import getImgUrl from "../../utils/getImgUrl";
+import { useForm } from "react-hook-form";
 
 const CheckoutPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const dispatch = useDispatch();
-
-  const handleRemoveFromCart = (item) => {
-    dispatch(removeFromCart(item));
-  };
-
   const total = cartItems
     .reduce((acc, item) => acc + item.newPrice, 0)
     .toFixed(2);
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  const currentUser = true; // TODO: get user from AUTH
+  const isChecked = true; // TODO : Do it From AUTH
 
   return (
-    <div className="flex mt-12 h-full flex-col overflow-hidden bg-white shadow-xl">
-      <div className="flex justify-between items-center p-4">
-        <h2 className="text-2xl font-semibold">Shopping Cart</h2>
-        <button onClick={() => dispatch(clearCart())} className="btn-primary">
-          Clear Cart
-        </button>
-      </div>
-      <div className="overflow-auto">
-        <div className="flex flex-col gap-2 p-4">
-          {cartItems.map((item, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-between hover:bg-gray-100 transition-all duration-200"
+    <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+      <div className="container max-w-screen-lg mx-auto">
+        <div>
+          <div>
+            <h2 className="font-semibold text-xl text-gray-600 mb-2">
+              Cash On Delevary
+            </h2>
+            <p className="text-gray-500 mb-2">Total Price: ${total}</p>
+            <p className="text-gray-500 mb-6">Items:{cartItems.length}</p>
+          </div>
+
+          <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3 my-8"
             >
-              <div className="flex items-center gap-4">
-                <img
-                  src={getImgUrl(item.coverImage)}
-                  alt={item.title}
-                  className="w-24 h-32 object-cover rounded-md"
-                />
-                <div className="">
-                  <Link to={`/books/${item._id}`}>
-                    <h3 className="text-xl font-semibold mb-1">{item.title}</h3>{" "}
-                    {/* Added mb-1 for spacing */}
-                  </Link>
-                  <p className="text-gray-600 mb-1">{item.author}</p>{" "}
-                  {/* Adjusted margin */}
-                  <p className="font-semibold">${item.newPrice}</p>
+              <div className="text-gray-600">
+                <p className="font-medium text-lg">Personal Details</p>
+                <p>Please fill out all the fields.</p>
+              </div>
+
+              <div className="lg:col-span-2">
+                <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 md:grid-cols-5">
+                  <div className="md:col-span-5">
+                    <label htmlFor="full_name">Full Name</label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                    />
+                  </div>
+
+                  <div className="md:col-span-5">
+                    <label html="email">Email Address</label>
+                    <input
+                      type="text"
+                      name="email"
+                      id="email"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      disabled
+                      defaultValue={currentUser?.email}
+                      placeholder="email@domain.com"
+                    />
+                  </div>
+                  <div className="md:col-span-5">
+                    <label html="phone">Phone Number</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      id="phone"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      placeholder="+123 456 7890"
+                    />
+                  </div>
+
+                  <div className="md:col-span-3">
+                    <label htmlFor="address">Address / Street</label>
+                    <input
+                      type="text"
+                      name="address"
+                      id="address"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      placeholder=""
+                    />
+                  </div>
+
+                  <div className="md:col-span-2">
+                    <label htmlFor="city">City</label>
+                    <input
+                      type="text"
+                      name="city"
+                      id="city"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      placeholder=""
+                    />
+                  </div>
+
+                  <div className="md:col-span-2">
+                    <label htmlFor="country">Country / region</label>
+                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                      <input
+                        name="country"
+                        id="country"
+                        placeholder="Country"
+                        className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                      />
+                      <button
+                        tabIndex="-1"
+                        className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600"
+                      >
+                        <svg
+                          className="w-4 h-4 mx-2 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <line x1="18" y1="6" x2="6" y2="18"></line>
+                          <line x1="6" y1="6" x2="18" y2="18"></line>
+                        </svg>
+                      </button>
+                      <button
+                        tabIndex="-1"
+                        className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
+                      >
+                        <svg
+                          className="w-4 h-4 mx-2 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <polyline points="18 15 12 9 6 15"></polyline>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="md:col-span-2">
+                    <label htmlFor="state">State / province</label>
+                    <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
+                      <input
+                        name="state"
+                        id="state"
+                        placeholder="State"
+                        className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                      />
+                      <button className="cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-red-600">
+                        <svg
+                          className="w-4 h-4 mx-2 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <line x1="18" y1="6" x2="6" y2="18"></line>
+                          <line x1="6" y1="6" x2="18" y2="18"></line>
+                        </svg>
+                      </button>
+                      <button
+                        tabIndex="-1"
+                        className="cursor-pointer outline-none focus:outline-none border-l border-gray-200 transition-all text-gray-300 hover:text-blue-600"
+                      >
+                        <svg
+                          className="w-4 h-4 mx-2 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <polyline points="18 15 12 9 6 15"></polyline>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="md:col-span-1">
+                    <label htmlFor="zipcode">Zipcode</label>
+                    <input
+                      type="text"
+                      name="zipcode"
+                      id="zipcode"
+                      className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      placeholder=""
+                    />
+                  </div>
+
+                  <div className="md:col-span-5 mt-3">
+                    <div className="inline-flex items-center">
+                      <input
+                        type="checkbox"
+                        name="billing_same"
+                        id="billing_same"
+                        className="form-checkbox"
+                      />
+                      <label htmlFor="billing_same" className="ml-2 ">
+                        I am aggree to the{" "}
+                        <Link className="underline underline-offset-2 text-blue-600">
+                          Terms & Conditions
+                        </Link>{" "}
+                        and{" "}
+                        <Link className="underline underline-offset-2 text-blue-600">
+                          Shoping Policy.
+                        </Link>
+                      </label>
+                    </div>
+                  </div>
+
+                  <div className="md:col-span-5 text-right">
+                    <div className="inline-flex items-end">
+                      <button
+                        disabled={!isChecked}
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                      >
+                        Place an Order
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <button
-                onClick={() => handleRemoveFromCart(item)}
-                className="btn-secondary"
-              >
-                Remove
-              </button>
-            </div>
-          ))}
+            </form>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-between p-4">
-        <h3 className="text-xl font-semibold">Total: ${total}</h3>
-        <Link to="/checkout" className="btn-primary">
-          Checkout
-        </Link>
       </div>
     </div>
   );
