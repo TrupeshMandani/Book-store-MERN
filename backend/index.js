20957a127057dbd5e350ac283d0856f873af960b
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-04 01:36:34 -0700

    "Added CORS middleware, updated book route to log request body, and fixed router initialization"

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5001; // Corrected environment variable casing
 
+// Middleware
+/* `app.use(cors());` is setting up Cross-Origin Resource Sharing (CORS) middleware in the Express
+application. This allows the server to handle requests from different origins. */
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json()); // Parse JSON bodies
+
 // Define a route
 const bookRoutes = require("./src/books/book.route");
-app.use("/api/books", (bookRoutes) => {
-  res.send("Book Store Server is running");
-});
+app.use("/api/books", bookRoutes);
 // Connect to MongoDB
 async function main() {
   try {
     await mongoose.connect("mongodb://localhost:27017/book-store-MERN");
     console.log("Connected to MongoDB"); // Log success message
+    app.use("/", (req, res) => res.send("Book server is running "));
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message); // Log error message
   }
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,19 +1,14 @@
 const express = require("express");
-const router = express.router();
+const router = express.Router();
 
 // post a book
-/* This code snippet is defining a POST route using Express. When a POST request is made to the
-specified route ("/"), it creates a new instance of a Book model with the data from the request
-body. It then attempts to save this new book to the database asynchronously. If the book is
-successfully saved, it responds with a status of 200 and sends the saved book as a JSON response. If
-there is an error during the saving process, it catches the error, responds with a status of 500,
-and sends the error as a JSON response. */
+
+/* The code `router.post("/create-book", async (req, res) => {
+  console.log(req.body);
+});` is defining a route in an Express application that listens for POST requests to the
+"/create-book" endpoint. When a POST request is made to this endpoint, the provided callback
+function is executed. */
 router.post("/create-book", async (req, res) => {
-  const newBook = new Book(req.body);
-  try {
-    const savedBook = await newBook.save();
-    res.status(200).json(savedBook);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  console.log(req.body);
 });
+module.exports = router;
