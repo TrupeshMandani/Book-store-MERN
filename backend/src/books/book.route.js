cec1b6122e037bd5f79e021aa308a5f861f34607
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-04 01:40:44 -0700

    "Added import statement for Book model and modified /create-book endpoint to create and save a new Book instance"

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+import { Book } from "./book.model.js";
 // post a book
 
 /* The code `router.post("/create-book", async (req, res) => {
@@ -9,6 +9,12 @@ const router = express.Router();
 "/create-book" endpoint. When a POST request is made to this endpoint, the provided callback
 function is executed. */
 router.post("/create-book", async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
+  try {
+    const newBook = await Book({ ...req.body });
+    await newBook.save();
+  } catch (error) {
+    console.log(error);
+  }
 });
 module.exports = router;
