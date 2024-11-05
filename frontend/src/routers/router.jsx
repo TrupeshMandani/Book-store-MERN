2478bab2db5e5a4f5e18218d6e850716ddfe495a
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-05 00:43:21 -0700

    "Added SingleBook route to router"

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -5,6 +5,7 @@ import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
 import CheckoutPage from "../pages/books/CheckoutPage";
+import SingleBook from "../pages/books/SIngleBook";
 
 const router = createBrowserRouter([
   {
@@ -40,6 +41,10 @@ const router = createBrowserRouter([
       {
         path: "/checkout",
         element: <CheckoutPage />,
+      },
+      {
+        path: "/books/:id",
+        element: <SingleBook />,
       },
     ],
   },
