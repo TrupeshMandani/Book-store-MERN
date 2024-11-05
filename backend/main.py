8a33f34a2df3288189539bdcd2775d571f216b8a
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-05 00:54:02 -0700

    "Deleted file backend/main.py"

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-# create function to fetch API 
-# create function to fetch API with query
-# create function to fetch API with query and filter
-
-import requests
-
-def fetch_api():
-    response = requests.get('https://api.publicapis.org/entries')
-    return response.json()
