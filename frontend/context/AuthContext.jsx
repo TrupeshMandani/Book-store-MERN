d74533878361c147427c1a5480f84ecb27aadee4
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-06 01:12:12 -0700

    "Added AuthProvider component to AuthContext"

--- frontend/context/AuthContext.jsx
+++ frontend/context/AuthContext.jsx
@@ -4,3 +4,9 @@ const AuthContext = createContext();
 export const useAuth = () => {
   return useContext(AuthContext);
 };
+
+//AuthProvider
+const AuthProvide = ({ children }) => {
+  const value = {};
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
