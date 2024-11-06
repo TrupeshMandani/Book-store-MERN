3536c443e480166495ea9319d2623e7a5fd8bc63
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-06 01:05:10 -0700

    "Updated Firebase config to use environment variables instead of hardcoded values"

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -9,7 +9,17 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+<<<<<<< HEAD
  // Necessary KEYS for Firabase configuration Which are not safe to publish 
+=======
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+>>>>>>> 7a3e438 ("Updated Firebase config to use environment variables instead of hardcoded values")
 };
 
 // Initialize Firebase
