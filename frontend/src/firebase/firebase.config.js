59e44040b44b4a8679fca6b699f43b963f4c313b
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-06 00:57:40 -0700

    Update firebase.config.js
    Removed the APIKEYS

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -9,13 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDNYkWZtEeKEWzZUqyCBf9YrrUSGpKOqIM",
-  authDomain: "book-store-mern-29dab.firebaseapp.com",
-  projectId: "book-store-mern-29dab",
-  storageBucket: "book-store-mern-29dab.firebasestorage.app",
-  messagingSenderId: "689583512268",
-  appId: "1:689583512268:web:71940c7d2bd76cbd730069",
-  measurementId: "G-4H76R529XV",
+ // Necessary KEYS for Firabase configuration Which are not safe to publish 
 };
 
 // Initialize Firebase
