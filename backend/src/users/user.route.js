a1cc00daeae88627973df627b7b953c9cb59a9b3
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-25 01:14:02 -0700

    "Updated user.route.js: added jwt, fixed typo in JWT_SECRET_KEY, changed error messages and status codes, and reformatted code"

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -1,39 +1,39 @@
 const express = require("express");
 const User = require("./user.model");
+const jwt = require("jsonwebtoken");
+
 const router = express.Router();
-const JWT_SECRET = process.env.JWT_SECRET_Key;
+
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
 router.post("/admin", async (req, res) => {
   const { username, password } = req.body;
   try {
     const admin = await User.findOne({ username });
     if (!admin) {
-      return res.status(404).json({ message: "Admin not found" });
+      res.status(404).send({ message: "Admin not found!" });
     }
-    if (username.password !== admin.password) {
-      res.status(404).json({ message: "password not match" });
+    if (admin.password !== password) {
+      res.status(401).send({ message: "Invalid password!" });
     }
+
     const token = jwt.sign(
-      {
-        id: admin._id,
-        username: admin.username,
-        role: admin.role,
-      },
+      { id: admin._id, username: admin.username, role: admin.role },
       JWT_SECRET,
-      { expireIN: "1h" }
+      { expiresIn: "1h" }
     );
+
     return res.status(200).json({
-      message: "authentican Successfully",
+      message: "Authentication successful",
       token: token,
       user: {
-        id: admin._id,
         username: admin.username,
         role: admin.role,
       },
     });
   } catch (error) {
-    console.error("faield to login as admin", error);
-    res.status(500).json({ message: "Failed to Login " });
+    console.error("Failed to login as admin", error);
+    res.status(401).send({ message: "Failed to login as admin" });
   }
 });
 
