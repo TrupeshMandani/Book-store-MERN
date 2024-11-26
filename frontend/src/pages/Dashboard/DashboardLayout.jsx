029ac4397b636df2dce5f0e9b1ea317665200757
TrupeshMandani <153320309+TrupeshMandani@users.noreply.github.com>
2024-11-26 15:02:17 -0700

    "Added useState and useEffect hooks to DashboardLayout component to fetch data from API"

--- frontend/src/pages/Dashboard/DashboardLayout.jsx
+++ frontend/src/pages/Dashboard/DashboardLayout.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getBaseURL } from "../../utils/baseURL";
 
 const DashboardLayout = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchdata = async () => {
+      try {
+        const response = await axios.get(`${getBaseURL}/api/admin`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setData(response.data);
+      } catch (error) {
+        console.error("Error:", error);
+        setLoading(false);
+      }
+    };
+  });
+  if (loading) return;
   return <div>DashboardLayout</div>;
 };
 
